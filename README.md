d0fb3649ec91c6da7c0410ba32f3da4abb5875af
kz.li <kz.li@hashicorp.com>
2025-05-28 09:28:44 +0800

    Updated to support requested structure

--- README.md
+++ README.md
@@ -23,56 +23,162 @@ export AWS_DEFAULT_REGION=REGION
 
 # Example output
 ```shell
-load_balancer_config = {
-    arn = "arn:aws:elasticloadbalancing:ap-southeast-1:058264549112:loadbalancer/net/likzv/99b9c61d4191f150"
-    listeners = [
-        {
-            arn = "arn:aws:elasticloadbalancing:ap-southeast-1:058264549112:listener/net/likzv/99b9c61d4191f150/185ad7398dec6f65",
-            port = "8200",
-            protocol = "TCP"
-            rules = [
-                arn = "arn:aws:elasticloadbalancing:ap-southeast-1:058264549112:listener-rule/net/likzv/99b9c61d4191f150/185ad7398dec6f65/197ebee75dc12de8"
-                target_group = {
-                    target_group_arn = "arn:aws:elasticloadbalancing:ap-southeast-1:058264549112:targetgroup/likzv-vault/1ee5660685f7cb58",
-                    type = "forward",
-                    order = "1"
-                }
+lb_config = {
+  "likzv" = {
+    lb_name = "likzv"
+    internal = True
+    load_balancer_type = "network"
+    subnet = ['subnet-050239ff37c785c2a']
+    tags = {
+      Project-Code = "CK"
+      Team = "SEC"
+      Name = "test"
+    }
+    deletion_protection = "false"
+    enable_cross_zone_load_balancing = "false"
+    vpc_id = "vpc-0958771d93cc3cc6c"
+    security_groups_ids = []
+
+    ip_address_type = "ipv4"
+    load_balancer_type = "network"
+    client_keep_alive = "3600"
+    customer_owned_ipv4_pool = "None"
+    desync_mitigation_mode = "defensive"
+    dns_record_client_routing_policy = "any_availability_zone"
+    drop_invalid_header_fields = "False"
+    enable_cross_zone_load_balancing = "false"
+    enable_deletion_protection = ""
+    enable_http2 = "True"
+    enable_tls_version_and_cipher_suite_headers = "False"
+    enable_waf_fail_open = "False"
+    enable_xff_client_port = "False"
+    xff_header_processing_mode = "False"
+    enable_zonal_shift = "false"
+    enforce_security_group_inbound_rules_on_private_link_traffic = "on"
+    idle_timeout = "60"
+    preserve_host_header = "False"
+
+    listeners = {
+      listener-8200 = {
+        port = "8200"
+        protocol = "TCP"
+        certificate_arn = ""
+        additional_certificate_arns = []
+        weighted_forward = [
+          {
+            target_group_key = "arn:aws:elasticloadbalancing:ap-southeast-1:123456789012:targetgroup/likzv-vault/1ee5660685f7cb58"
+            target_groups = [
+              {
+                target_group_key = "arn:aws:elasticloadbalancing:ap-southeast-1:123456789012:targetgroup/likzv-vault/1ee5660685f7cb58"
+                weight = None
+              }
             ]
-            certificate_arns = [
+          }
+        ]
+      }
+      listener-443 = {
+        port = "443"
+        protocol = "TLS"
+        certificate_arn = ""
+        additional_certificate_arns = ['arn:aws:acm:ap-southeast-1:123456789012:certificate/2fec407d-821f-4228-8c40-716a77b437f6', 'arn:aws:acm:ap-southeast-1:123456789012:certificate/2d2f0cb1-c08d-4e9e-9de2-a2aef02cd665', 'arn:aws:acm:ap-southeast-1:123456789012:certificate/a7134534-82d5-4fba-920a-76dd920178fa']
+        weighted_forward = [
+          {
+            target_group_key = "arn:aws:elasticloadbalancing:ap-southeast-1:123456789012:targetgroup/likzv-vault/1ee5660685f7cb58"
+            target_groups = [
+              {
+                target_group_key = "arn:aws:elasticloadbalancing:ap-southeast-1:123456789012:targetgroup/likzv-vault/1ee5660685f7cb58"
+                weight = None
+              }
             ]
-        }, 
-        {
-            arn = "arn:aws:elasticloadbalancing:ap-southeast-1:058264549112:listener/net/likzv/99b9c61d4191f150/1d5fdd3d18f06f78",
-            port = "443",
-            protocol = "TLS"
-            rules = [
-                arn = "arn:aws:elasticloadbalancing:ap-southeast-1:058264549112:listener-rule/net/likzv/99b9c61d4191f150/1d5fdd3d18f06f78/818caaadc3cfcf75"
-                target_group = {
-                    target_group_arn = "arn:aws:elasticloadbalancing:ap-southeast-1:058264549112:targetgroup/likzv-vault/1ee5660685f7cb58",
-                    type = "forward",
-                    order = "None"
-                }
+          }
+        ]
+      }
+      listener-5696 = {
+        port = "5696"
+        protocol = "TCP"
+        certificate_arn = ""
+        additional_certificate_arns = []
+        weighted_forward = [
+          {
+            target_group_key = "arn:aws:elasticloadbalancing:ap-southeast-1:123456789012:targetgroup/likzv-vault-kmip/e379c7749487ec8e"
+            target_groups = [
+              {
+                target_group_key = "arn:aws:elasticloadbalancing:ap-southeast-1:123456789012:targetgroup/likzv-vault-kmip/e379c7749487ec8e"
+                weight = None
+              }
             ]
-            certificate_arns = [
-                "arn:aws:acm:ap-southeast-1:058264549112:certificate/2d2f0cb1-c08d-4e9e-9de2-a2aef02cd665", 
-                "arn:aws:acm:ap-southeast-1:058264549112:certificate/a7134534-82d5-4fba-920a-76dd920178fa"
-            ]
-        }, 
-        {
-            arn = "arn:aws:elasticloadbalancing:ap-southeast-1:058264549112:listener/net/likzv/99b9c61d4191f150/f5744a6ae66847aa",
-            port = "5696",
-            protocol = "TCP"
-            rules = [
-                arn = "arn:aws:elasticloadbalancing:ap-southeast-1:058264549112:listener-rule/net/likzv/99b9c61d4191f150/f5744a6ae66847aa/299d68adf96053c7"
-                target_group = {
-                    target_group_arn = "arn:aws:elasticloadbalancing:ap-southeast-1:058264549112:targetgroup/likzv-vault-kmip/e379c7749487ec8e",
-                    type = "forward",
-                    order = "None"
-                }
-            ]
-            certificate_arns = [
-            ]
+          }
+        ]
+      }
+    }
+  }
+  "test" = {
+    lb_name = "test"
+    internal = False
+    load_balancer_type = "application"
+    subnet = ['subnet-0bd0945130fb0ce5f', 'subnet-0d8007e17249aa3cc']
+    tags = {
+    }
+    deletion_protection = "false"
+    enable_cross_zone_load_balancing = "true"
+    vpc_id = "vpc-0958771d93cc3cc6c"
+    security_groups_ids = ['sg-03b051e17aa170fcc']
+
+    ip_address_type = "ipv4"
+    load_balancer_type = "application"
+    client_keep_alive = "3600"
+    customer_owned_ipv4_pool = "None"
+    desync_mitigation_mode = "defensive"
+    dns_record_client_routing_policy = "any_availability_zone"
+    drop_invalid_header_fields = "false"
+    enable_cross_zone_load_balancing = "true"
+    enable_deletion_protection = ""
+    enable_http2 = "true"
+    enable_tls_version_and_cipher_suite_headers = "false"
+    enable_waf_fail_open = "false"
+    enable_xff_client_port = "false"
+    xff_header_processing_mode = "append"
+    enable_zonal_shift = "false"
+    enforce_security_group_inbound_rules_on_private_link_traffic = "on"
+    idle_timeout = "60"
+    preserve_host_header = "false"
+
+    listeners = {
+      listener-81 = {
+        port = "81"
+        protocol = "HTTP"
+        certificate_arn = ""
+        additional_certificate_arns = []
+        default_action = {
+          fixed-response = {
+            content_type = "text/plain"
+            message_body = "test"
+            status_code = "503"
+          }
         }
-    ]
+      }
+      listener-80 = {
+        port = "80"
+        protocol = "HTTP"
+        certificate_arn = ""
+        additional_certificate_arns = []
+        weighted_forward = [
+          {
+            target_group_key = "None"
+            target_groups = [
+              {
+                target_group_key = "arn:aws:elasticloadbalancing:ap-southeast-1:123456789012:targetgroup/test3/2b6309c644b07bc4"
+                weight = 1
+              }, 
+              {
+                target_group_key = "arn:aws:elasticloadbalancing:ap-southeast-1:123456789012:targetgroup/test/fbb09289b209396d"
+                weight = 1
+              }
+            ]
+          }
+        ]
+      }
+    }
+  }
 }
 ```
--- main.py
+++ main.py
@@ -2,6 +2,11 @@ import boto3
 from jinja2 import Environment, FileSystemLoader
 
 
+def get_attribute_value(lb_attributes, key, return_if_none=None):
+    value = next((d["Value"] for d in lb_attributes.get("Attributes") if d["Key"] == key), None)
+    return value if value else return_if_none
+
+
 def get_all_lb_attributes():
     compiled_load_balancers = {
         "load_balancers": []
@@ -15,44 +20,113 @@ def get_all_lb_attributes():
     # Iterate through all the load balancers within the account
     for lb in load_balancers:
         lb_arn = lb["LoadBalancerArn"]
-        formatted_lb_attributes = {"arn": lb_arn, "listeners": []}
+        is_internal = True if lb.get("Scheme") == "internet-facing" else False
+        is_alb = True if lb.get("Type") == "application" else False
+        # Get LB tags
+        lb_tags = elbv2.describe_tags(ResourceArns=[lb_arn])
+        # Get LB attributes
+        lb_attributes = elbv2.describe_load_balancer_attributes(LoadBalancerArn=lb_arn)
+        # print(lb_attributes.get("Attributes"))
+
+        formatted_lb_attributes = {
+            "arn": lb_arn,
+            "lb_name": lb.get("LoadBalancerName"),
+            "internal": is_internal,
+            "load_balancer_type": lb.get("Type"),
+            "vpc_id": lb.get("VpcId"),
+            "subnets": [az.get("SubnetId") for az in lb.get("AvailabilityZones")],
+            "security_groups_ids": lb.get("SecurityGroups", []),
+            "ip_address_type": lb.get("IpAddressType"),
+            "enforce_security_group_inbound_rules_on_private_link_traffic": lb.get(
+                "EnforceSecurityGroupInboundRulesOnPrivateLinkTraffic", "on"),
+            # Only applicable to NLB but seems like sample structure has this attribute so we cater for it
+            "customer_owned_ipv4_pool": lb.get("CustomerOwnedIpv4Pool") if lb.get("CustomerOwnedIpv4Pool") else None,
+            "deletion_protection": get_attribute_value(lb_attributes, "deletion_protection.enabled", False),
+            "enable_cross_zone_load_balancing": get_attribute_value(lb_attributes, "load_balancing.cross_zone.enabled",
+                                                                    True if is_alb else False),
+            "client_keep_alive": get_attribute_value(lb_attributes, "client_keep_alive.seconds", 3600),
+            "desync_mitigation_mode": get_attribute_value(lb_attributes, "routing.http.desync_mitigation_mode",
+                                                          "defensive"),
+            "dns_record_client_routing_policy": get_attribute_value(lb_attributes, "dns_record.client_routing_policy",
+                                                                    "any_availability_zone"),
+            "drop_invalid_header_fields": get_attribute_value(lb_attributes,
+                                                              "routing.http.drop_invalid_header_fields.enabled", False),
+            "enable_http2": get_attribute_value(lb_attributes, "routing.http2.enabled", True),
+            "enable_tls_version_and_cipher_suite_headers": get_attribute_value(lb_attributes,
+                                                                               "routing.http.x_amzn_tls_version_and_cipher_suite.enabled",
+                                                                               False),
+            "enable_waf_fail_open": get_attribute_value(lb_attributes, "waf.fail_open.enabled", False),
+            "enable_xff_client_port": get_attribute_value(lb_attributes, "routing.http.xff_client_port.enabled", False),
+            "xff_header_processing_mode": get_attribute_value(lb_attributes, "routing.http.xff_header_processing.mode",
+                                                              False),
+            "enable_zonal_shift": get_attribute_value(lb_attributes, "zonal_shift.config.enabled", False),
+            "idle_timeout": get_attribute_value(lb_attributes, "idle_timeout.timeout_seconds", 60),
+            "preserve_host_header": get_attribute_value(lb_attributes, "routing.http.preserve_host_header.enabled",
+                                                        False),
+            "listeners": [],
+            "tags": [{"key": tag.get("Key"), "value": tag.get("Value")} for tag in
+                     lb_tags.get("TagDescriptions", [{"Tags": {}}])[0].get("Tags", [])]
+        }
 
         lb_listeners = elbv2.describe_listeners(LoadBalancerArn=lb_arn)
 
         # For each LB, get the listener information
         for listener in lb_listeners.get("Listeners"):
-            print(listener)
-            lb_listener_arn = listener["ListenerArn"]
-            formatted_lb_listener = {
-                "arn": lb_listener_arn,
+            formatted_listener_config = {
                 "port": listener.get("Port"),
                 "protocol": listener.get("Protocol"),
-                "ssl_policy": str(listener.get("SslPolicy")),
-                "rules": [],
-                "certificates": []
             }
+            lb_listener_arn = listener["ListenerArn"]
 
             # Get listener rules
             lb_listener_rules = elbv2.describe_rules(ListenerArn=lb_listener_arn)
+            weighted_listener_rules = []
+            default_action_rule = {}
+
             for rule in lb_listener_rules.get("Rules"):
-                formatted_lb_listener_rule = {
-                    "arn": rule.get("RuleArn"),
-                    "actions": [
-                        {
-                            "type": action.get("Type"),
-                            "TargetGroupArn": action.get("TargetGroupArn"),
-                            "Order": str(action.get("Order"))
-                        } for action in rule.get("Actions")]
-                }
-                formatted_lb_listener.get("rules").append(formatted_lb_listener_rule)
+                print(rule)
+                for action in rule.get("Actions"):
+                    if action.get("ForwardConfig"):
+                        weighted_listener_rules.append(
+                            {
+                                "target_group_key": action.get("TargetGroupArn"),
+                                "target_groups": [{
+                                    "target_group_key": tg.get("TargetGroupArn"),
+                                    "weight": tg.get("Weight")
+                                } for tg in action.get("ForwardConfig").get("TargetGroups")]
+                            }
+                        )
+                    if action.get("FixedResponseConfig"):
+                        default_action_rule = {
+                            action.get("Type"): {
+                                "content_type": action.get("FixedResponseConfig").get("ContentType"),
+                                "message_body": action.get("FixedResponseConfig").get("MessageBody"),
+                                "status_code": action.get("FixedResponseConfig").get("StatusCode")
+                            }
+                        }
+
+            if weighted_listener_rules:
+                formatted_listener_config["weighted_forward"] = weighted_listener_rules
+            if default_action_rule:
+                formatted_listener_config["default_action"] = default_action_rule
 
             # Get listener certs
             lb_listener_certs = elbv2.describe_listener_certificates(ListenerArn=lb_listener_arn)
-            for lb_listener_cert in lb_listener_certs.get("Certificates", []):
-                formatted_lb_listener_cert = {"CertificateArn": lb_listener_cert.get("CertificateArn")}
+            certificates = lb_listener_certs.get("Certificates")
+            certificate_arn = "None"
+            additional_certificate_arns = []
 
-                if formatted_lb_listener_cert:
-                    formatted_lb_listener["certificates"].append(formatted_lb_listener_cert)
+            if certificates:
+                certificate_arn = next(
+                    (cert["CertificateArn"] for cert in certificates if cert["IsDefault"]))  # Default certificate
+                additional_certificate_arns = [cert["CertificateArn"] for cert in certificates if not cert[
+                    "IsDefault"]]  # Any additional certificates attached to the listener
+            formatted_listener_config["certificate_arn"] = certificate_arn
+            formatted_listener_config["additional_certificate_arns"] = additional_certificate_arns
+
+            formatted_lb_listener = {
+                f"listener-{listener.get('Port')}": formatted_listener_config
+            }
 
             formatted_lb_attributes["listeners"].append(formatted_lb_listener)
         compiled_load_balancers["load_balancers"].append(formatted_lb_attributes)
@@ -68,4 +142,7 @@ def render_template(lb_info):
 
 
 lb_info = get_all_lb_attributes()
+f = open("render.json", "w")
+f.write(str(lb_info))
+f.close()
 render_template(lb_info)
