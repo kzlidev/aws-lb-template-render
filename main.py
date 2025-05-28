d5444771e41538a926b842ef0932db13e9535c69
kz.li <kz.li@hashicorp.com>
2025-05-28 11:14:47 +0800

    Move formatting from direct API calls to using a audit file

--- main.py
+++ main.py
@@ -7,25 +7,64 @@ def get_attribute_value(lb_attributes, key, return_if_none=None):
     return value if value else return_if_none
 
 
+def write_file(file_name, content):
+    f = open(file_name, "w")
+    f.write(str(content))
+    f.close()
+
+
 def get_all_lb_attributes():
-    compiled_load_balancers = {
-        "load_balancers": []
-    }
+    response_audit_dump = []
     # Create a client for ELBv2 (Application and Network Load Balancers)
     elbv2 = boto3.client("elbv2")
     # Retrieve all load balancers
     response = elbv2.describe_load_balancers()
     load_balancers = response.get("LoadBalancers")
-
     # Iterate through all the load balancers within the account
     for lb in load_balancers:
+        lb_arn = lb["LoadBalancerArn"]
+        lb_attributes = elbv2.describe_load_balancer_attributes(LoadBalancerArn=lb_arn)
+        # Get LB tags
+        lb_tags = elbv2.describe_tags(ResourceArns=[lb_arn])
+        audit_dump_lb_attributes = lb
+        audit_dump_lb_attributes["Attributes"] = lb_attributes
+        audit_dump_lb_attributes["Tags"] = lb_tags
+        audit_dump_lb_attributes["Listeners"] = []
+        lb_listeners = elbv2.describe_listeners(LoadBalancerArn=lb_arn)
+
+        # For each LB, get the listener information
+        for listener in lb_listeners.get("Listeners"):
+            lb_listener_arn = listener["ListenerArn"]
+            # Get listener rules
+            lb_listener_rules = elbv2.describe_rules(ListenerArn=lb_listener_arn)
+            # Get listener certs
+            lb_listener_certs = elbv2.describe_listener_certificates(ListenerArn=lb_listener_arn)
+
+            audit_dump_lb_attributes["Listeners"].append({
+                "Attributes": listener,
+                "Rules": lb_listener_rules,
+                "Certificates": lb_listener_certs
+            })
+
+        response_audit_dump.append(audit_dump_lb_attributes)
+    write_file("audit.json", str(response_audit_dump))
+    return response_audit_dump
+
+
+def format_lb_attributes(load_balancers_info):
+    compiled_load_balancers = {
+        "load_balancers": []
+    }
+
+    # Iterate through all the load balancers within the account
+    for lb in load_balancers_info:
         lb_arn = lb["LoadBalancerArn"]
         is_internal = True if lb.get("Scheme") == "internet-facing" else False
         is_alb = True if lb.get("Type") == "application" else False
         # Get LB tags
-        lb_tags = elbv2.describe_tags(ResourceArns=[lb_arn])
+        lb_tags = lb.get("Tags")
         # Get LB attributes
-        lb_attributes = elbv2.describe_load_balancer_attributes(LoadBalancerArn=lb_arn)
+        lb_attributes = lb.get("Attributes")
         # print(lb_attributes.get("Attributes"))
 
         formatted_lb_attributes = {
@@ -37,46 +76,41 @@ def get_all_lb_attributes():
             "subnets": [az.get("SubnetId") for az in lb.get("AvailabilityZones")],
             "security_groups_ids": lb.get("SecurityGroups", []),
             "ip_address_type": lb.get("IpAddressType"),
-            "enforce_security_group_inbound_rules_on_private_link_traffic": lb.get( "EnforceSecurityGroupInboundRulesOnPrivateLinkTraffic", "on"),
+            "enforce_security_group_inbound_rules_on_private_link_traffic": lb.get("EnforceSecurityGroupInboundRulesOnPrivateLinkTraffic", "on"),
             # Only applicable to NLB but seems like sample structure has this attribute so we cater for it
             "customer_owned_ipv4_pool": lb.get("CustomerOwnedIpv4Pool") if lb.get("CustomerOwnedIpv4Pool") else None,
-            "deletion_protection": get_attribute_value(lb_attributes, "deletion_protection.enabled", False),
-            "enable_cross_zone_load_balancing": get_attribute_value(lb_attributes, "load_balancing.cross_zone.enabled", True if is_alb else False),
+            "enable_deletion_protection": get_attribute_value(lb_attributes, "deletion_protection.enabled", "false"),
+            "enable_cross_zone_load_balancing": get_attribute_value(lb_attributes, "load_balancing.cross_zone.enabled", "true" if is_alb else "false"),
             "client_keep_alive": get_attribute_value(lb_attributes, "client_keep_alive.seconds", 3600),
             "desync_mitigation_mode": get_attribute_value(lb_attributes, "routing.http.desync_mitigation_mode", "defensive"),
             "dns_record_client_routing_policy": get_attribute_value(lb_attributes, "dns_record.client_routing_policy", "any_availability_zone"),
-            "drop_invalid_header_fields": get_attribute_value(lb_attributes, "routing.http.drop_invalid_header_fields.enabled", False),
-            "enable_http2": get_attribute_value(lb_attributes, "routing.http2.enabled", True),
-            "enable_tls_version_and_cipher_suite_headers": get_attribute_value(lb_attributes, "routing.http.x_amzn_tls_version_and_cipher_suite.enabled", False),
-            "enable_waf_fail_open": get_attribute_value(lb_attributes, "waf.fail_open.enabled", False),
-            "enable_xff_client_port": get_attribute_value(lb_attributes, "routing.http.xff_client_port.enabled", False),
-            "xff_header_processing_mode": get_attribute_value(lb_attributes, "routing.http.xff_header_processing.mode", False),
-            "enable_zonal_shift": get_attribute_value(lb_attributes, "zonal_shift.config.enabled", False),
+            "drop_invalid_header_fields": get_attribute_value(lb_attributes, "routing.http.drop_invalid_header_fields.enabled", "false"),
+            "enable_http2": get_attribute_value(lb_attributes, "routing.http2.enabled", "true"),
+            "enable_tls_version_and_cipher_suite_headers": get_attribute_value(lb_attributes, "routing.http.x_amzn_tls_version_and_cipher_suite.enabled", "false"),
+            "enable_waf_fail_open": get_attribute_value(lb_attributes, "waf.fail_open.enabled", "false"),
+            "enable_xff_client_port": get_attribute_value(lb_attributes, "routing.http.xff_client_port.enabled", "false"),
+            "xff_header_processing_mode": get_attribute_value(lb_attributes, "routing.http.xff_header_processing.mode", "false"),
+            "enable_zonal_shift": get_attribute_value(lb_attributes, "zonal_shift.config.enabled", "false"),
             "idle_timeout": get_attribute_value(lb_attributes, "idle_timeout.timeout_seconds", 60),
-            "preserve_host_header": get_attribute_value(lb_attributes, "routing.http.preserve_host_header.enabled",
-                                                        False),
+            "preserve_host_header": get_attribute_value(lb_attributes, "routing.http.preserve_host_header.enabled", "false"),
             "listeners": [],
-            "tags": [{"key": tag.get("Key"), "value": tag.get("Value")} for tag in
-                     lb_tags.get("TagDescriptions", [{"Tags": {}}])[0].get("Tags", [])]
+            "tags": [{"key": tag.get("Key"), "value": tag.get("Value")} for tag in lb_tags.get("TagDescriptions", [{"Tags": {}}])[0].get("Tags", [])]
         }
 
-        lb_listeners = elbv2.describe_listeners(LoadBalancerArn=lb_arn)
-
+        lb_listeners = lb.get("Listeners")
         # For each LB, get the listener information
-        for listener in lb_listeners.get("Listeners"):
+        for listener_info in lb_listeners:
+            listener = listener_info.get("Attributes")
             formatted_listener_config = {
                 "port": listener.get("Port"),
                 "protocol": listener.get("Protocol"),
             }
-            lb_listener_arn = listener["ListenerArn"]
 
             # Get listener rules
-            lb_listener_rules = elbv2.describe_rules(ListenerArn=lb_listener_arn)
             weighted_listener_rules = []
             default_action_rule = {}
 
-            for rule in lb_listener_rules.get("Rules"):
-                print(rule)
+            for rule in listener_info.get("Rules").get("Rules"):
                 for action in rule.get("Actions"):
                     if action.get("ForwardConfig"):
                         weighted_listener_rules.append(
@@ -103,14 +137,14 @@ def get_all_lb_attributes():
                 formatted_listener_config["default_action"] = default_action_rule
 
             # Get listener certs
-            lb_listener_certs = elbv2.describe_listener_certificates(ListenerArn=lb_listener_arn)
-            certificates = lb_listener_certs.get("Certificates")
+            certificates = listener_info.get("Certificates").get("Certificates")
             certificate_arn = "None"
             additional_certificate_arns = []
 
             if certificates:
                 certificate_arn = next((cert["CertificateArn"] for cert in certificates if cert["IsDefault"]))  # Default certificate
-                additional_certificate_arns = [cert["CertificateArn"] for cert in certificates if not cert["IsDefault"]]  # Any additional certificates attached to the listener
+                additional_certificate_arns = [cert["CertificateArn"] for cert in certificates if not cert[
+                    "IsDefault"]]  # Any additional certificates attached to the listener
 
             formatted_listener_config["certificate_arn"] = certificate_arn
             formatted_listener_config["additional_certificate_arns"] = additional_certificate_arns
@@ -123,6 +157,7 @@ def get_all_lb_attributes():
 
         compiled_load_balancers["load_balancers"].append(formatted_lb_attributes)
 
+    write_file("render.json", str(compiled_load_balancers))
     return compiled_load_balancers
 
 
@@ -131,10 +166,9 @@ def render_template(lb_info):
     template = env.get_template('tfvars.template')
     output = template.render(lb_info)
     print(output)
+    return output
 
 
-lb_info = get_all_lb_attributes()
-f = open("render.json", "w")
-f.write(str(lb_info))
-f.close()
-render_template(lb_info)
+raw_info = get_all_lb_attributes()
+lb_info = format_lb_attributes(raw_info)
+write_file("example.tfvars", render_template(lb_info))
